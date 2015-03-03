6cbd57c0d917d5850220e4309166056a3641ca5f
Jonas Karlsson <jonas.karlsson@stim.se>
2015-03-03 08:41:54 +0100

    Added tests and implementation for Finland

--- finland/finland.go
+++ finland/finland.go
@@ -2,6 +2,9 @@ package finland
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
+	"time"
 
 	"github.com/steevel/idno-parsers"
 )
@@ -10,6 +13,89 @@ func init() {
 	parsers.Register("finland", Parse)
 }
 
+const (
+	ctrlChrs = "0123456789ABCDEFHJKLMNPRSTUVWXY"
+	reTpl    = "^(\\d{6})([-+A])(\\d{3})([%s])$"
+)
+
+var (
+	reID = regexp.MustCompile(fmt.Sprintf(reTpl, ctrlChrs))
+)
+
+// Parse id
+/*
+ * Accepts one format
+ *  - DDMMYY[+-A]FFFC
+ */
 func Parse(id string) (*parsers.IdNo, error) {
-	return nil, fmt.Errorf("Not implemented yet")
+	var idNo = parsers.New(id)
+
+	// Parse and see if valid
+	match := reID.FindAllStringSubmatch(id, -1)
+	if len(match) == 0 {
+		return idNo, parsers.ErrInvalidFormat
+	}
+
+	// Extract the date, century, increment and check character
+	idDate := match[0][1]
+	idCent := match[0][2]
+	idIncr := match[0][3]
+	idCtrl := match[0][4]
+
+	// Combine date of birth with incremental number, divide by 31 to get
+	// check character
+	dateAndIncr, _ := strconv.Atoi(idDate + idIncr)
+	ctrlNum := dateAndIncr % 31
+	ctrlChr := ctrlChrs[ctrlNum : ctrlNum+1]
+
+	// Check authenticity
+	if ctrlChr != idCtrl {
+		return idNo, parsers.ErrInvalidCheckdigit
+	}
+
+	year, month, day, err := parseDate(idDate, idCent)
+	if err != nil {
+		return idNo, err
+	}
+
+	// The incremental number is uneven for men and even for women
+	gender := parsers.Male
+	if i, _ := strconv.Atoi(idIncr); i%2 == 0 {
+		gender = parsers.Female
+	}
+
+	// Everything looks OK, go ahead and populate return value
+	idNo.Valid = true
+	idNo.Data["gender"] = gender
+	idNo.Data["year"] = year
+	idNo.Data["month"] = month
+	idNo.Data["day"] = day
+
+	return idNo, nil
+}
+
+// Calculate century and extract year, month and day from the date
+func parseDate(date string, centMark string) (year string, month string, day string, err error) {
+	var century string
+
+	switch centMark {
+	case "+":
+		century = "18"
+	case "-":
+		century = "19"
+	case "A":
+		century = "20"
+	}
+
+	day = date[:2]
+	month = date[2:4]
+	year = century + date[4:6]
+
+	// Sanity check, is this a correct date?
+	dateString := fmt.Sprintf("%s-%s-%s", year, month, day)
+	if _, e := time.Parse("2006-01-02", dateString); e != nil {
+		err = parsers.ErrInvalidDate
+	}
+
+	return
 }
--- finland/finland_test.go
+++ finland/finland_test.go
@@ -3,34 +3,25 @@ package finland_test
 import (
 	"testing"
 
-	"github.com/steevel/idno-parsers/norway"
+	"github.com/steevel/idno-parsers/finland"
 )
 
 func TestValid(t *testing.T) {
-	t.Parallel()
-
-	tests := []struct {
-		in  string
-		out string
-	}{
-		{"08128149946", norway.Regular},
-	}
+	tests := []string{"311280-888Y"}
 
 	for _, tt := range tests {
-		if p, _ := norway.Parse(tt.in); !p.Valid || p.Data["type"] != tt.out {
-			t.Errorf("Invalid idno, Expected %v Got %v", p.Data["type"], tt.out)
+		if p, err := finland.Parse(tt); err != nil {
+			t.Errorf("Fail at '%v', out '%+v', err: %+v", tt, p, err)
 		}
 	}
 }
 
 func TestInvalid(t *testing.T) {
-	t.Parallel()
-
-	tests := []string{"08128149945", "08128149947"}
+	tests := []string{"311280-888X"}
 
 	for _, tt := range tests {
-		if p, _ := norway.Parse(tt); p.Valid {
-			t.Errorf("Valid idno, Expected an invalid")
+		if p, err := finland.Parse(tt); err == nil {
+			t.Errorf("Fail at '%v', out '%+v', should have produced error", tt, p)
 		}
 	}
 }
